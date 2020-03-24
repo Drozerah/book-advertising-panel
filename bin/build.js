229c5575aa0a2ff4266e2f2417086dc9d51c9785
Drozerah <drozerah@gmail.com>
2020-03-24 19:49:14 +0100

    feat: add example

--- bin/build.js
+++ bin/build.js
@@ -5,26 +5,35 @@
 /**
  * Node Modules
  */
-// eslint-disable-next-line no-unused-vars
 const fs = require('fs')
 const rimraf = require('rimraf')
 const { execSync } = require('child_process')
 
 /**
- * Tasks
+ * Build Tasks
  */
 // remove distFolder
 rimraf.sync('book-panel')
 // create book-panel folder
 execSync('mkdirp book-panel')
+// create book-panel/js subfolder
 execSync('mkdirp book-panel/js')
+// create book-panel/css subfolder
 execSync('mkdirp book-panel/css')
+// create book-panel/img subfolder
 execSync('mkdirp book-panel/img')
+// import/copy img files to book-panel folder
 execSync('cp -r src/img book-panel')
+// import/copy minifyed css file to book-panel folder
 execSync('cp src/css/main.min.css book-panel/css')
+// read file
 const data = fs.readFileSync('book-panel/css/main.min.css', 'utf-8')
-const regex = new RegExp(/\/\*# sourceMappingURL=main.min.css.map \*\//)
-const newValue = data.replace(regex, '')
+// remove string from data
+const newValue = data.replace(new RegExp(/\/\*# sourceMappingURL=main.min.css.map \*\//), '')
+// write css file with newValue
 fs.writeFileSync('book-panel/css/main.min.css', newValue, 'utf-8')
+// rename css file
 fs.renameSync('book-panel/css/main.min.css', 'book-panel/css/book-panel.min.css')
+// run babel transpilator
 execSync('npm run babel')
+console.log('build done!')
